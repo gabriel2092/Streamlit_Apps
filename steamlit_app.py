92a332897e6311bb5983e126f13c6eda9ef8edf8
gabriel2092 <gabrieltrejos20@gmail.com>
2023-10-19 12:22:29 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -6,4 +6,5 @@ streamlit.text('🥣Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard Boiled Egg')
 streamlit.text('🥑Avocado Toast')
- 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
