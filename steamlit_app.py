d093ebd4d64ff64421abf741bbff06edbc38b22e
gabriel2092 <gabrieltrejos20@gmail.com>
2023-10-19 12:48:08 -0600

    Update steamlit_app.py
    Actually filter the fruits

--- steamlit_app.py
+++ steamlit_app.py
@@ -17,5 +17,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Put in a multiselect to pick the fruit
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(my_fruit_list)
