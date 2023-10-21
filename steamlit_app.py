062e3a36ec4c4a0a14e5b9759e98f9ff705e7a9d
gabriel2092 <gabrieltrejos20@gmail.com>
2023-10-21 09:43:33 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -28,3 +28,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+#create function to normalize json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Creates dataframe of normalized fruit.
+streamlit.dataframe(fruityvice_normalized)
