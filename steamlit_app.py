8a90d459ef0b3ecd4ade49b3b9b6d59352ff7a8a
gabriel2092 <gabrieltrejos20@gmail.com>
2023-10-21 09:54:35 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -24,9 +24,13 @@ streamlit.dataframe(fruits_to_show)
 #Header Fruit Advice
 streamlit.header("Fruityvice Fruit Advice!")
 
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #new section to display API response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 streamlit.text(fruityvice_response)
 
 #create function to normalize json
@@ -34,5 +38,3 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Creates dataframe of normalized fruit.
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
