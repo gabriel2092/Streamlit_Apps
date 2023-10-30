c0b8e3ecfd8db2142eeb3698947cb29c28d1272a
gabriel2092 <gabrieltrejos20@gmail.com>
2023-10-30 08:27:51 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,7 @@
 import snowflake.connector
 import streamlit
+import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -11,7 +13,7 @@ streamlit.text('🥑Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #need to correct indez 
@@ -31,7 +33,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 #new section to display API response
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response)
 
