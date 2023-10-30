e93c5e1be2c539707bbc0d4362d5f61448d606dd
gabriel2092 <gabrieltrejos20@gmail.com>
2023-10-29 21:02:02 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,11 +1,6 @@
 import snowflake.connector
 import streamlit
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
 
 
 streamlit.title('My Parents New Healthy Dinner')
@@ -47,3 +42,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Creates dataframe of normalized fruit.
 streamlit.dataframe(fruityvice_normalized)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
