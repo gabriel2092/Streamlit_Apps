5cf31618577708abbec912b864dea4272846d19f
gabriel2092 <gabrieltrejos20@gmail.com>
2023-10-29 21:19:28 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -47,7 +47,6 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
 streamlit.dataframe(my_data_row)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
